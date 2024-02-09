05c8c64ae209f7387c7847ff7329bc1266c7988c
TejasTank1 <tejas.tank@zeuslearning.com>
2024-02-09 10:56:09 +0530

    done

--- src/app/Modules/login/login.routing.module.ts
+++ src/app/Modules/login/login.routing.module.ts
@@ -4,6 +4,7 @@ import { LogincompComponent } from './logincomp/logincomp.component';
 import { MainComponent } from './Create_account/main/main.component';
 
 const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
   {path:'login',component:LogincompComponent},
   {path:'createaccount',component:MainComponent}
 ];
